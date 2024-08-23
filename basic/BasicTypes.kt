a66b505842d5553ece2187f38c54159afc6fdd12
Iron-Buster <1457480465@qq.com>
2024-08-23 12:29:16 +0800

    Create BasicTypes.kt

---
+++ basic/BasicTypes.kt
@@ -0,0 +1,15 @@
+fun main() {
+//    val d: Int
+//    d = 3
+//    val s: String = "hello"
+//
+//    println(d)
+//    println(s)
+
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long = 1000000007
+    var e: Boolean = false
+    var f: Char = '\n'
+}
