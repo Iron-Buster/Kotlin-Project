ef368008efb515df4ed4c8e9b20a4b37bc7833da
Iron-Buster <1457480465@qq.com>
2024-08-23 12:29:51 +0800

    Create StringTemplates.kt

---
+++ basic/StringTemplates.kt
@@ -0,0 +1,14 @@
+
+fun main() {
+    val a = 10
+    println("a is $a")
+    println("a add one is ${a + 1}")
+
+    // Complete the code to make the program print "Mary is 20 years old" to standard
+    fun practice() {
+        val name = "Mary"
+        val age = 20
+        println("$name is $age years old")
+    }
+    practice()
+}
